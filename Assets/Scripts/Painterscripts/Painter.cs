8242f1afa78115c01061ba8b219ff2a25c60283c
Adroan Covari Heinen <31225010+Adroan@users.noreply.github.com>
2021-05-09 01:19:59 -0300

    Android support added

--- Assets/Scripts/Painterscripts/Painter.cs
+++ Assets/Scripts/Painterscripts/Painter.cs
@@ -20,31 +20,66 @@ public class Painter : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(Application.platform);
+        if(Application.platform == RuntimePlatform.Android){
+            if(Input.touchCount == 1){
+                Vector3 touchPos = Input.GetTouch(0).position;
+                if(Input.GetTouch(0).phase == TouchPhase.Began && ((touchPos.x >= xLimitMin + transform.position.x && touchPos.x <= xLimitMax + transform.position.x) && (touchPos.y <= yLimitMin + transform.position.y && touchPos.y >= yLimitMax+ transform.position.y))){
+                    StartCoroutine(painterAndroid());
+                }else if(Input.GetTouch(0).phase == TouchPhase.Ended || !((touchPos.x >= xLimitMin + transform.position.x && touchPos.x <= xLimitMax + transform.position.x) && (touchPos.y <= yLimitMin + transform.position.y && touchPos.y >= yLimitMax+ transform.position.y))){
+                    StopCoroutine(painterAndroid());
+                    if(positions != null &&!player.hasLeg)
+                        player.setLegPosCells(positions);
+                    StartCoroutine(eraser());
+                    StopCoroutine(eraser());
+                }
 
-        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            }
+        }else if(Application.platform == RuntimePlatform.WebGLPlayer || Application.platform == RuntimePlatform.WindowsEditor){
+          Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         if (Input.GetMouseButtonDown(0) && ((mousePos.x >= xLimitMin + transform.position.x && mousePos.x <= xLimitMax + transform.position.x) && (mousePos.y <= yLimitMin + transform.position.y && mousePos.y >= yLimitMax+ transform.position.y)))
         {
 
-            StartCoroutine(painter());
+            StartCoroutine(painterWeb());
         }
         else if (Input.GetMouseButtonUp(0) || !((mousePos.x >= xLimitMin + transform.position.x && mousePos.x <= xLimitMax+ transform.position.x) && (mousePos.y <= yLimitMin + transform.position.y && mousePos.y >= yLimitMax + transform.position.y)))
         {
 
-            StopCoroutine(painter());
+            StopCoroutine(painterWeb());
             if (positions != null && !player.hasLeg)
                 player.setLegPosCells(positions);
             StartCoroutine(eraser());
             StopCoroutine(eraser());
 
+        }  
         }
+        
 
     }
 
+    IEnumerator painterAndroid(){
+        line = new GameObject().AddComponent<LineRenderer>();
+        line.transform.SetParent(transform);
+        line.name = "leg";
+        positions = new List<Vector3>();
+        line.startWidth = 0.1f;
+        line.endWidth = 0.1f;
+        line.material = lineColor;
+        player.hasLeg =false;
 
+         while (Input.GetTouch(0).phase != TouchPhase.Ended && line != null)
+        {
+            positions.Add(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position) + Vector3.forward * 4.5f);
+            line.positionCount = positions.Count;
+            line.SetPositions(positions.ToArray());
 
-    IEnumerator painter()
+            yield return new WaitForSeconds(0);
+
+        }
+    }
+
+    IEnumerator painterWeb()
     {
         line = new GameObject().AddComponent<LineRenderer>();
         line.transform.SetParent(transform);
